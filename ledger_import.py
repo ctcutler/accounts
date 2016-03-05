f8c29011a59f63c764df26cc54e37fc48258c467
ctcutler <ctcutler@gmail.com>
2016-03-05 08:03:22 -0500

    read accounts from file, plan future changes

--- ledger_import.py
+++ ledger_import.py
@@ -1,39 +1,36 @@
+import sys
 from cmd import Cmd
 from random import choice
 
 # For tab completion in MacOS X, from:
-# (https://pewpewthespells.com/blog/osx_readline.html)
+# https://pewpewthespells.com/blog/osx_readline.html
 import readline
 readline.parse_and_bind("bind ^I rl_complete")
 
 class LedgerImportCmd(Cmd):
     """
     TODO:
-    * suggest account
+    * read transactions from existing .dat file
+    * make dict of all known desc -> account pairs
+    * read lines from input file
+        * look up and suggest account based on desc of every line
+    * print new dat file on quit
+        * print new all accounts
+        * print all transactions
     """
-    COMPLETIONS = [
-        'Expenses',
-        'Expenses:Watches',
-        'Expenses:Watches:Straps',
-        'Expenses:Watches:Maintenance',
-        'Income',
-        'Income:Salary',
-        'Income:Salary:A Salary',
-        'Income:Salary:C Salary',
-        'Liabilities',
-        'Assets',
-        'Equity',
-    ]
+    accounts = []
 
     def set_suggestion(self):
-        self.prompt = 'Enter Account [{}]: '.format(choice(self.COMPLETIONS))
+        self.prompt = 'Enter Account [{}]: '.format(choice(self.accounts))
 
     def default(self, line):
         print line
+        if line not in self.accounts:
+            self.accounts.append(line)
         self.set_suggestion()
 
     def completenames(self, text, line, begidx, endidx):
-        return [c for c in self.COMPLETIONS if c.startswith(text)]
+        return [c for c in self.accounts if c.startswith(text)]
 
     def emptyline(self):
         self.set_suggestion()
@@ -41,9 +38,17 @@ class LedgerImportCmd(Cmd):
     def do_EOF(self, line):
         return True
 
+def read_accounts(fn):
+    with open(fn) as f:
+        return [
+            line.split(' ', 1)[1].strip()
+            for line in f.readlines()
+            if line.startswith('account ')
+        ]
 
 def main():
     cmd = LedgerImportCmd()
+    cmd.accounts = read_accounts(sys.argv[1])
     cmd.set_suggestion()
     cmd.cmdloop()
 
