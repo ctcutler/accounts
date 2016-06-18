5921a661519d0eea2b1006f46b1ca66eca50efd4
ctcutler <ctcutler@gmail.com>
2016-06-18 11:45:21 -0400

    refactor splitN helpers

--- js/src/util.js
+++ js/src/util.js
@@ -8,18 +8,11 @@ export const trace = R.curry((tag, x) => {
 });
 
 const search = R.invoker(1, 'search');
-const substring = R.invoker(2, 'substring');
-const substr = R.invoker(1, 'substr');
-const lSubstr = i => substring(0, i);
-const rSubstr = R.curry(
-  (i, s) => R.compose(
-    substr,
-    R.subtract(i+1),
-    R.length
-  )(s)(s)
-);
-const lSplit = R.curry((pat, s) => R.compose(lSubstr, search(pat))(s)(s));
-const rSplit = R.curry((pat, s) => R.compose(rSubstr, search(pat))(s)(s));
+const substring2 = R.invoker(2, 'substring');
+const substring1 = R.invoker(1, 'substring');
+const twice = f => d => f(d)(d);
+const lSplit = pat => twice(R.compose(substring2(0), search(pat)));
+const rSplit = pat => twice(R.compose(substring1, R.add(1), search(pat)));
 const escapeRe = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 const result = R.nth(1);
 const capture = R.compose(R.match, RegExp, s => `^[${s}]*(.*)`, escapeRe);
@@ -32,7 +25,7 @@ export const splitN = R.curry(
   (pat, n, s) =>
     R.test(pat, s) && n > 0
       ? R.compose(
-          R.prepend(lSplit(pat, s)),
+          R.prepend(lSplit(pat)(s)),
           splitN(pat, n-1),
           rSplit(pat)
         )(s)
