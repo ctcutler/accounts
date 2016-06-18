664976fcff10358a5ef89a47c039111af8e6a1d7
ctcutler <ctcutler@gmail.com>
2016-06-18 18:35:49 -0400

    refactor parsing to be more (sigh) FP

--- js/src/parse.js
+++ js/src/parse.js
@@ -2,29 +2,13 @@ const R = require('ramda');
 const Decimal = require('decimal.js');
 import { splitN, lTrim, trace, applyIfTruthy } from './util';
 
-export const parseTransaction = input => {
-  let lines = R.split('\n')(input);
-  let [date, desc] = R.compose(splitN(/ /, 1), R.head)(lines);
-
-
-  const safeDecimal = applyIfTruthy(Decimal);
-
-  let postings = R.compose(
-    R.map(
-      R.compose(
-        parts => ({
-          'name': R.trim(parts[1]),
-          'quantity': safeDecimal(lTrim('$ ')(parts[2]))
-        }),
-        splitN(/\s{2,}/, 2)
-      )
-    ),
-    R.tail
-  )(lines);
-
-  return {
-    desc,
-    date: new Date(date),
-    postings,
-  };
-}
+const lines = R.split('\n');
+const firstLine = R.compose(R.head, lines);
+const safeDecimal = applyIfTruthy(Decimal);
+const desc = R.compose(R.last, splitN(/ /, 1), firstLine);
+const date = R.compose(R.constructN(1, Date), R.head, splitN(/ /, 1), firstLine);
+const name = R.compose(R.trim, R.nth(1));
+const quantity = R.compose(safeDecimal, lTrim('$ '), R.nth(2));
+const posting = R.compose(R.applySpec({ name, quantity }), splitN(/\s{2,}/, 2));
+const postings = R.compose(R.map(posting), R.tail, lines);
+export const parseTransaction = R.applySpec({ desc, date, postings });
