1b0bf995c813c795e2c32fcdcffade41c3bd75c0
ctcutler <ctcutler@gmail.com>
2016-07-03 12:52:23 -0400

    first pass at analysis

---
+++ js/src/analyze.js
@@ -0,0 +1,36 @@
+Error.stackTraceLimit = Infinity;
+const R = require('ramda');
+import { trace } from './util';
+
+export const mergeAmounts = R.mergeWith(R.add);
+export const amount = R.compose(
+  R.ifElse(
+    R.isEmpty,
+    R.identity,
+    o => R.objOf(o.commodity, o.quantity)
+  ),
+  R.prop('amount')
+);
+export const reducePosting = (acc, p) => R.mergeWith(
+  mergeAmounts, R.objOf(p['account'], amount(p)), acc
+);
+const invertValues = R.map(R.multiply(-1));
+export const balancedAmount = R.compose(
+  R.reduce(mergeAmounts, {}),
+  R.map(invertValues),
+  R.values,
+  R.filter(R.compose(R.not, R.isEmpty))
+);
+export const emptyKey = R.compose(R.head, R.keys, R.filter(R.isEmpty));
+export const balancePostings = mapping => R.assoc(
+  emptyKey(mapping),
+  balancedAmount(mapping),
+  mapping
+);
+const reduceTrans = (acc, v) => R.compose(
+  R.mergeWith(mergeAmounts, acc),
+  balancePostings,
+  R.reduce(reducePosting, {}), // make account -> {commodity -> quantity} mapping
+  R.prop('postings')
+)(v);
+export const balance = R.reduce(reduceTrans, {});
---
+++ js/test/analyze.spec.js
@@ -0,0 +1,106 @@
+const R = require('ramda');
+import { balance, balancePostings, mergeAmounts, amount,
+         balancedAmount, emptyKey } from '../src/analyze';
+
+const transactions = [
+  {
+    desc: 'grocery store!',
+    date: new Date('2014/02/14'),
+    postings: [
+      {
+        account: 'Liabilities:Credit Cards:MasterCard',
+        amount: { quantity: -34.52 , commodity: '$' }
+      },
+      {
+        account: 'Expenses:Groceries',
+        amount: {}
+      }
+    ]
+  },
+  {
+    desc: 'pay credit card bill',
+    date: new Date('2014/02/19'),
+    postings: [
+      {
+        account: 'Assets:Bank Account:National Savings Bank',
+        amount: { quantity: -34.52 , commodity: '$' }
+      },
+      {
+        account: 'Liabilities:Credit Cards:MasterCard',
+        amount: {}
+      }
+    ]
+  }
+];
+describe('amount', function() {
+  it('should return a commodity -> quantity object', () => {
+    const input = {amount: { quantity: -34.52 , commodity: '$' }};
+    expect(amount(input)['$']).toEqual(-34.52);
+  });
+
+  it('should return a completely empty object', () => {
+    const input = {amount: {}};
+    expect(R.isEmpty(amount(input))).toBe(true);
+  });
+});
+
+const accountMapping = {
+  'Account 1:Subaccount 1': {'$': -34.52},
+  'Account 2': {'$': -1.23},
+  'Account 3': {'FOOBAR': -123.45},
+  'Account 4': {}
+};
+
+describe('emptyKey', function () {
+  it('should return the key that corresponds to an empty object', function () {
+    expect(emptyKey(accountMapping)).toEqual('Account 4');
+  });
+});
+
+describe('balancedAmount', function () {
+  it('should return the balanced amount', function () {
+    expect(balancedAmount(accountMapping)).toEqual({'$': 35.75, 'FOOBAR': 123.45});
+  });
+});
+
+describe('balancePostings', function () {
+  it('should balance postings', function () {
+    const balanced = balancePostings(accountMapping);
+    expect(balanced['Account 4']['$']).toEqual(35.75);
+    expect(balanced['Account 4']['FOOBAR']).toEqual(123.45);
+  });
+});
+
+describe('mergeAmounts', function () {
+  it('should properly merge different commodities', function () {
+    const merged = mergeAmounts(
+      {'FOOBAR': 11.11},
+      {'BAZBUZ': 11.11}
+    );
+    expect(merged.FOOBAR).toEqual(11.11);
+    expect(merged.BAZBUZ).toEqual(11.11);
+  });
+  it('should properly merge the same commodity', function () {
+    const merged = mergeAmounts(
+      {'FOOBAR': 11.11},
+      {'FOOBAR': 11.11}
+    );
+    expect(merged.FOOBAR).toEqual(R.add(11.11, 11.11));
+  });
+});
+
+describe('balance', function () {
+  it('should return the right credit card balance', function () {
+    expect(balance(transactions)['Liabilities:Credit Cards:MasterCard'])
+      .toEqual({$: 0.00});
+  });
+
+  it('should return the right groceries balance', function () {
+    expect(balance(transactions)['Expenses:Groceries']).toEqual({'$': 34.52});
+  });
+
+  it('should return the right bank account balance', function () {
+    expect(balance(transactions)['Assets:Bank Account:National Savings Bank'])
+      .toEqual({$: -34.52});
+  });
+});
