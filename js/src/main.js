6221a9be3b72c69ae220d951e97d9d6b8f860fbf
ctcutler <ctcutler@gmail.com>
2016-09-11 12:42:50 -0400

    first time series chart

--- js/src/main.js
+++ js/src/main.js
@@ -1,6 +1,6 @@
 import { data } from './data';
 import { balance, toDollars, filterAccount, filterBefore, filterAfter,
-  balanceTransactions, convertTransactions
+  balanceTransactions, convertTransactions, overMonths, overDays
 } from './analyze';
 import { ledger } from './parse';
 import { trace } from './util';
@@ -22,9 +22,20 @@ const columns = R.compose(
   filterAfter(new Date('2016/01/01')),
   R.prop('transactions')
 )(ledgerData);
+const timeSeries = R.compose(
+  pairs => [R.pluck(0, pairs), R.pluck(1, pairs)],
+  R.prepend(['x', 'data1']),
+  overDays(/^Assets/),
+  convertTransactions('$', ledgerData['commodityPrices']),
+  balanceTransactions,
+  filterBefore(new Date('2016/03/01')),
+  filterAfter(new Date('2016/01/01')),
+  R.prop('transactions')
+)(ledgerData);
 
 const main = <div>
-  <div id="chart"></div>
+  <div id="pieChart"></div>
+  <div id="timeSeriesChart"></div>
 </div>;
 
 ReactDOM.render(
@@ -32,13 +43,25 @@ ReactDOM.render(
   document.getElementById('example')
 );
 
-const chart = c3.generate({
-    bindto: '#chart',
+const pieChart = c3.generate({
+    bindto: '#pieChart',
     size: {
       height: 700
     },
     data: {
       type: 'pie',
       columns
+    }
+});
+const timeSeriesChart = c3.generate({
+    bindto: '#timeSeriesChart',
+    data: {
+        x: 'x',
+        columns: timeSeries
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+        }
     }
 });
