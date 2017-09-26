0e34aa529121deebf49d072b8877f57a74ec1478
ctcutler <ctcutler@gmail.com>
2017-09-25 21:50:51 -0400

    fun with averages

--- src/components/SavingRate.js
+++ src/components/SavingRate.js
@@ -18,6 +18,12 @@ class SavingRate extends React.Component {
     const addCommas = v => v.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
     // FIXME: write function to normalize time ranges rather than hard coding
     // that R.drop(4). . . be sure to normalize *after* calculating running totals
+    const yearAverages = R.compose(
+      R.map(R.mean),
+      R.reduce(R.mergeWith(R.concat), {}),
+      R.map(x => R.objOf(x[0].getYear().toString(), [x[1]]))
+    );
+    const averageSeries = (avgs, series) => R.map(x => avgs[x[0].getYear()], series);
     const incomeSeries = R.compose(
       R.dropLast(1), // because last will be incomplete
       R.drop(4),
@@ -25,6 +31,10 @@ class SavingRate extends React.Component {
       R.map(R.adjust(invertDecimal, 1)),
       overMonths(/^Income/)
     )(transactions);
+
+    const incomeAverages = yearAverages(incomeSeries);
+    const averageIncomeSeries = averageSeries(incomeAverages, incomeSeries);
+
     const expensesSeries = R.compose(
       R.dropLast(1), // because last will be incomplete
       fillInMonths,
@@ -32,9 +42,16 @@ class SavingRate extends React.Component {
       overMonths(/^Expenses/)
     )(transactions);
 
+    const expensesAverages = yearAverages(expensesSeries);
+    const averageExpensesSeries = averageSeries(expensesAverages, expensesSeries);
+
     const savingRateSeries = R.zipWith(
-      (x, y) => R.when(v => v === undefined, v => null, addDecimal(x[1], y[1]))
+      (x, y) => R.when(v => v === undefined, v => null, [x[0], addDecimal(x[1], y[1])])
     )(incomeSeries, expensesSeries);
+
+    const savingAverages = yearAverages(savingRateSeries);
+    const averageSavingSeries = averageSeries(savingAverages, savingRateSeries);
+    const pluckedSavingRateSeries = R.pluck(1, savingRateSeries);
 
     // FIXME: abstract better and move to analyze.js
     // base on the formula found here: http://math.stackexchange.com/questions/204020/what-is-the-equation-used-to-calculate-a-linear-trendline/204021#204021
@@ -52,31 +69,40 @@ class SavingRate extends React.Component {
       );
     const lrOffset = (xs, ys, slope) =>
       (R.sum(ys) - (slope * R.sum(xs))) / xs.length;
-    const rateIndexes = R.range(0, savingRateSeries.length);
-    const m = lrSlope(rateIndexes, savingRateSeries);
-    const b = lrOffset(rateIndexes, savingRateSeries, m);
+    const rateIndexes = R.range(0, pluckedSavingRateSeries.length);
+    const m = lrSlope(rateIndexes, pluckedSavingRateSeries);
+    const b = lrOffset(rateIndexes, pluckedSavingRateSeries, m);
     const trendSeries = R.map(R.compose(R.add(b), R.multiply(m)), rateIndexes);
     const columns = [
         R.prepend('x', R.pluck(0, incomeSeries)),
         R.prepend('Income', R.pluck(1, incomeSeries)),
         R.prepend('Expenses', R.pluck(1, expensesSeries)),
-        R.prepend('savingRate', savingRateSeries),
-        R.prepend('trend', trendSeries)
+        R.prepend('savingRate', pluckedSavingRateSeries),
+        R.prepend('trend', trendSeries),
+        R.prepend('AverageIncome', averageIncomeSeries),
+        R.prepend('AverageExpenses', averageExpensesSeries),
+        R.prepend('AverageSavings', averageSavingSeries)
     ];
     const data = {
       x: 'x',
       columns,
       types: {
-        Income: 'area',
-        Expenses: 'area',
+        Income: 'line',
+        Expenses: 'line',
         savingRate: 'line',
-        trend: 'line'
+        trend: 'line',
+        AverageIncome: 'area',
+        AverageExpenses: 'area',
+        AverageSavings: 'area',
       },
       colors: {
         Income: '#079400',
         Expenses: '#DD0000',
         savingRate: '#000000',
         trend: '#0000FF',
+        AverageIncome: '#079400',
+        AverageExpenses: '#DD0000',
+        AverageSavings: '#000000',
       }
     };
 
